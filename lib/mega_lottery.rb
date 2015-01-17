3ba7563918324061f0b3ab7ef9034f97adffdd62
Derick Hitchcock <hitchcock.derick@gmail.com>
2015-01-17 17:06:27 -0500

    Rename MegaLotto to MegaLottery in order to be unique on RubyGems.org

---
+++ lib/mega_lottery.rb
@@ -0,0 +1,11 @@
+require "mega_lottery/version"
+require "mega_lottery/drawing"
+
+begin
+  require "pry" 
+rescue LoadError 
+end
+
+module MegaLottery
+  # binding.pry
+end
---
+++ lib/mega_lottery/version.rb
@@ -0,0 +1,3 @@
+module MegaLottery
+  VERSION = "0.0.1"
+end
